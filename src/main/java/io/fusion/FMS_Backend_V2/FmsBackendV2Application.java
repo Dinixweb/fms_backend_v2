e8e788e6005be612aeec82b6e6cb2e4b5e60b2b4
Dennis <sd4live@gmail.com>
2022-12-03 15:20:42 +0100

    updates

---
+++ src/main/java/io/fusion/FMS_Backend_V2/FmsBackendV2Application.java
@@ -0,0 +1,13 @@
+package io.fusion.FMS_Backend_V2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmsBackendV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FmsBackendV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/io/fusion/FMS_Backend_V2/FmsBackendV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.fusion.FMS_Backend_V2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FmsBackendV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
